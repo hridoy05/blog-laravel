1aa5c6fee2ff3a2014cad770185da5569b461c6a
Hridoy Banik <hridoy.banik71@gmail.com>
2023-03-30 09:32:30 +0600

    add: add controller

---
+++ database/migrations/2023_03_30_031346_create_settings_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('settings', function (Blueprint $table) {
+            $table->id();
+            $table->string('comment_auto')->default(0);
+            $table->string('user_auto')->default(0);
+            $table->string('recent_limit')->default(5);
+            $table->string('popular_limit')->default(5);
+            $table->string('recent_comment_limit')->default(5);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('settings');
+    }
+};
