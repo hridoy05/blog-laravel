242fe616101fde65d8becccfce1cfbd6a20d886b
Hridoy Banik <hridoy.banik71@gmail.com>
2023-03-30 19:23:13 +0600

    add: router added

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,23 +2,31 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\View;
 use Illuminate\Support\ServiceProvider;
-
+use Illuminate\Pagination\Paginator;
 class AppServiceProvider extends ServiceProvider
 {
     /**
      * Register any application services.
+     *
+     * @return void
      */
-    public function register(): void
+    public function register()
     {
         //
     }
 
     /**
      * Bootstrap any application services.
+     *
+     * @return void
      */
-    public function boot(): void
+    public function boot()
     {
-        //
+        $setting=\App\Models\Setting::first();
+        Paginator::useBootstrap();
+        View::share('recent_posts',\App\Models\Post::orderBy('id','desc')->limit($setting->recent_limit)->get());
+        View::share('popular_posts',\App\Models\Post::orderBy('views','desc')->limit($setting->popular_limit)->get());
     }
 }
